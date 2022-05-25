4c43587bc40f3de479594a196c727a4015af6b35
Derek Todd <derekhtodd@yahoo.co.uk>
2022-05-25 15:33:28 +0100

    First version of initialise board

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+def initGameData():
+
+    gameData = {
+        "board": {
+            "h": "RNBQKBNR",
+            "g": "PPPPPPPP",
+            "f": "        ",
+            "e": "        ",
+            "d": "        ",
+            "c": "        ",
+            "b": "pppppppp",
+            "a": "rnbqkbnr"
+        }
+    }
+
+    return(gameData)
+
+
+
+
+
+
+def main():
+    gameData = initGameData()
