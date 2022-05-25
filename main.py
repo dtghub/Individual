a9169bbb4e33603cb492b5441419393776d13bb2
Derek Todd <derekhtodd@yahoo.co.uk>
2022-05-25 19:20:55 +0100

    Woring on "enter square to move from" code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 
+from operator import truediv
+
+
 def initGameData():
     gameData = {
         "board": [
@@ -16,16 +19,29 @@ def initGameData():
     return(gameData)
 
 
+def getColumnLetters():
+    return("abcdefgh")
 
-def displayBoard(gameData):
+def getRowNumberString():
+    return("12345678")
 
-    columnLetters = "   abcdefgh\n"
+
+def convertLetterToColumNumber(columLetter):
+    listOfColumnLetters = getColumnLetters()
+    columnNumber = listOfColumnLetters.index(columLetter)
+    return(columnNumber)
+
+
+
+def displayBoard(boardToDisplay):
+
+    columnLetters = "   " + getColumnLetters() + "\n"
     rowSeparator = "   --------\n"
     boardOutput = columnLetters + rowSeparator
 
 
     for rowNumber in range(0,8):
-        boardOutput += str(rowNumber) + " |" + gameData["board"][rowNumber] + "\n"
+        boardOutput += str(rowNumber) + " |" + boardToDisplay[rowNumber] + "| " + str(rowNumber) + "\n"
 
     boardOutput += rowSeparator + columnLetters
 
@@ -37,10 +53,82 @@ def displayBoard(gameData):
 
 
 
+def evaluateCoordinatesEntered(coordinatesEntered):
+    isValidInput = False
+    columnLetters = getColumnLetters()
+    rowNumbersString = getRowNumberString()
+    if len(coordinatesEntered) == 2:
+        if (coordinatesEntered[0] in columnLetters) and (coordinatesEntered[1] in rowNumbersString):
+            isValidInput = True
+    return(isValidInput)
+
+
+
+
+def getCoordinatesFromPlayer(textToDisplay):
+    playerCoordinates = input(textToDisplay)
+    isValidInput = evaluateCoordinatesEntered(playerCoordinates)
+    return(isValidInput, playerCoordinates)
+
+
+
+
+def getValueAtPositionOnBoard(coordinate, boardToCheck):
+    columNumber = convertLetterToColumNumber(coordinate[0])
+    valueAtPosition = boardToCheck[int(coordinate[1])][columNumber]
+    return(valueAtPosition)
+
+
+
+
+
+
+
+def getPlayerMove(gameData):
+    isGameInProgress = True
+
+    while isGameInProgress:
+
+        
+        promptText = "Please input the coordinates to move from: "
+        isValidInput, coordinatesEntered = getCoordinatesFromPlayer(promptText)
+        if isValidInput and (len(coordinatesEntered) == 2):
+            valueAtPositionOnBoard = getValueAtPositionOnBoard(coordinatesEntered, gameData["board"])
+            if valueAtPositionOnBoard == ".":
+                isChessPieceOnSquare = False
+            else:
+                isChessPieceOnSquare = True
+
+            
+
+
+
+
+    return isGameInProgress, gameData 
+
+
+
+
+
+
+
+
 
 def main():
     gameData = initGameData()
-    displayBoard(gameData)
+
+
+    isGameInProgress = True
+    while isGameInProgress:
+
+
+        displayBoard(gameData["board"])
+        isGameInProgress, gameData = getPlayerMove(gameData)
+
+
+
+
+
 
 
 main()
